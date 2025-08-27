3aab12b897724ed78d73e1ec233582ddbf8b0a53
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-08-27 22:13:32 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/mymath/Main.java
@@ -0,0 +1,23 @@
+// Java program to find maximum value from two given
+
+package com.mymath;
+
+// Main class
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Print maximum value of given
+        System.out.println("Max from 2.1 and 3.9 is " + Math.max(2.1, 3.9));
+
+        // Print maximum value of given
+        System.out.println("Max from 10.2 and 40.6 is " + Math.max(10.2, 40.6));
+
+        // Print maximum value of given
+        System.out.println("Max from 70.4 and 40.6 is " + Math.max(70.4, 40.6));
+
+        // Print maximum value of given
+        System.out.println("Max from 70.4 and 1330.9 is " + Math.max(70.4, 1330.9));
+    }
+}
